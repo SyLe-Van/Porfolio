83e9a64d355683dafa71d0eeef57eb3df14d79a9
Sy Le <sycung9001@gmail.com>
2024-07-15 13:41:54 +0700

    updated

--- .history/src/data/headerData_20240715134139.js
+++ .history/src/data/headerData_20240715134139.js
@@ -6,6 +6,6 @@ export const headerData = {
   desciption:
     "Welcome to my portfolio! Here, you'll find my projects, achievements, and skills in the field of technology. From coding projects on GitHub to professional highlights on LinkedIn, this platform offers a glimpse into my journey as an IT student. Explore how my passion for technology translates into practical solutions and collaborative work",
   image:
-    "https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-1/393003204_1514111392683548_4206460713441802419_n.jpg?stp=cp6_dst-jpg_p480x480&_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=vxyJ57DDijcQ7kNvgHiZdSL&_nc_ht=scontent.fsgn2-6.fna&cb_e2o_trans=q&oh=00_AYC7kgd6AGu52l-9OzwyGPKgVT_NkYN_m5Efcgk7-IZlWA&oe=666C2686",
+    "https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-1/393003204_1514111392683548_4206460713441802419_n.jpg?stp=cp6_dst-jpg_p480x480&_nc_cat=111&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=QJ60ZgL3W8IQ7kNvgFuMSbS&_nc_ht=scontent.fsgn2-6.fna&cb_e2o_trans=q&oh=00_AYB0lw_gISBvaEUdyU9AxRwVYUnGQIFEDQr0sEMm0jCbRg&oe=669A8346",
   resumePdf: resume,
 };
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -6,6 +6,6 @@ export const headerData = {
   desciption:
     "Welcome to my portfolio! Here, you'll find my projects, achievements, and skills in the field of technology. From coding projects on GitHub to professional highlights on LinkedIn, this platform offers a glimpse into my journey as an IT student. Explore how my passion for technology translates into practical solutions and collaborative work",
   image:
-    "https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-1/393003204_1514111392683548_4206460713441802419_n.jpg?stp=cp6_dst-jpg_p480x480&_nc_cat=111&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=n2l1MglPONYQ7kNvgE0WG61&_nc_ht=scontent.fsgn2-6.fna&cb_e2o_trans=q&oh=00_AYCCygJVAoJd3_pwShXFrd__bsrWNDXtqGxdTgrTR6zt7A&oe=668B21C6",
+    "https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-1/393003204_1514111392683548_4206460713441802419_n.jpg?stp=cp6_dst-jpg_p480x480&_nc_cat=111&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=QJ60ZgL3W8IQ7kNvgFuMSbS&_nc_ht=scontent.fsgn2-6.fna&cb_e2o_trans=q&oh=00_AYB0lw_gISBvaEUdyU9AxRwVYUnGQIFEDQr0sEMm0jCbRg&oe=669A8346",
   resumePdf: resume,
 };
